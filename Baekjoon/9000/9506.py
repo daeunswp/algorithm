e194b0bb91ea7389cef263bdd027c4ae6727c9a3
daeunswp <daeunswp@gmail.com>
2024-07-07 20:36:58 +0900

    #9506 약수들의 합

--- Baekjoon/9000/9506.py
+++ Baekjoon/9000/9506.py
@@ -1,1 +1,27 @@
 # 약수들의 합
+numList = []
+
+while(True):
+    num = int(input())
+    if(num == -1):
+        break
+    numList.append(num)
+
+for n in numList:
+    total = 1
+    divisor = '= 1'
+    for i in range(2, n):
+        if(n%i==0):
+            total += i 
+            divisor += ' + '
+            divisor += str(i)
+        else:
+            continue
+       
+    if(total == n):
+        print(n, divisor)
+    else:
+        print(n, "is NOT perfect.")
+        
+        
+            
